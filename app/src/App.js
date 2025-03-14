e63d50ba0020b1b32a212475e2c7b14e700f9b4e
multicolores <florian.tellier02@gmail.com>
2025-03-14 10:27:09 +0100

    feat: Create orders form and list pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,29 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import OdersListPage from "./pages/OdersListPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Passer une commande</Link>
+            </li>
+            <li>
+              <Link to="/second">Liste des commands</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/second" element={<OdersListPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ app/src/pages/OdersListPage.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+
+function OdersListPage() {
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  const fetchOrders = async () => {
+    try {
+      const response = await fetch(
+        "https://europe-west9-ue-dev-cloud.cloudfunctions.net/quarkus-api"
+      );
+      if (!response.ok) {
+        throw new Error("Erreur lors de la récupération des commandes");
+      }
+      const data = await response.json();
+
+      setOrders(data);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
+  };
+
+  if (loading) return <div>Chargement...</div>;
+  if (error) return <div>Erreur: {error}</div>;
+
+  return (
+    <div className="OrdersListPage">
+      <h1>Liste des Commandes</h1>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Nom</th>
+              <th>Adresse</th>
+              <th>Produit</th>
+              <th>Date de la commande</th>
+              <th>Date de livraison</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order, index) => (
+              <tr key={index}>
+                <td>{order.name}</td>
+                <td>{order.address}</td>
+                <td>{order.product}</td>
+                <td>{new Date(order.orderDate).toLocaleDateString()}</td>
+                <td>{new Date(order.deliveryDate).toLocaleDateString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default OdersListPage;
